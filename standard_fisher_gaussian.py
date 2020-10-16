ea27b3fc5f65da28386ca72afcf56dcd0bc08b46
Andy Jones <ajones788@gmail.com>
2020-10-16 10:20:35 -0500

    small plot change in main

--- standard_fisher_gaussian.py
+++ standard_fisher_gaussian.py
@@ -4,48 +4,48 @@ import numpy as np
 
 class StandardFisherGaussian:
 
-    def __init__(self, p, sigma2):
-        self.p = p
-        self.sigma2 = sigma2
+	def __init__(self, p, sigma2):
+		self.p = p
+		self.sigma2 = sigma2
 
-    # evaluate density
-    def pdf(self, x):
-        # TODO: write down density function
-        pass
+	# evaluate density
+	def pdf(self, x):
+		# TODO: write down density function
+		pass
 
-    # sample
-    def sample(self, n):
+	# sample
+	def sample(self, n):
 
-        # Sample from uniform sphere
-        from scipy.stats import multivariate_normal as mvn
-        gaussian_samples = mvn.rvs(mean=np.zeros(self.p), cov=1, size=n)
+		# Sample from uniform sphere by drawing from isotropic gaussian
+		#    and normalizing by L2 norm
+		from scipy.stats import multivariate_normal as mvn
+		gaussian_samples = mvn.rvs(mean=np.zeros(self.p), cov=1, size=n)
+		norms = np.linalg.norm(gaussian_samples, ord=2, axis=1)
+		sphere_samples = gaussian_samples / norms[:, None]
 
-        # Normalize by L2 norm of each sample
-        norms = np.linalg.norm(gaussian_samples, ord=2, axis=1)
-        sphere_samples = gaussian_samples / norms[:, None]
+		# Add gaussian noise with variance sigma2
+		noise = mvn.rvs(mean=np.zeros(self.p), cov=self.sigma2, size=n)
+		samples = sphere_samples + noise
 
-        # Add isotropic gaussian noise
-        noise = mvn.rvs(mean=np.zeros(self.p), cov=self.sigma2, size=n)
-        samples = sphere_samples + noise
-
-        return samples
+		return samples
 
 
 if __name__ == "__main__":
 	import matplotlib.pyplot as plt
 
-    n = 100
-    p = 2
+	n = 1000
+	p = 2
 
-    plt.figure(figsize=(7, 7))
-    sigma2_range = [np.power(10.0, x) for x in np.arange(-2, 2)]
-    for ii, sigma2 in enumerate(sigma2_range):
-        sfg = StandardFisherGaussian(p=p, sigma2=sigma2)
-        sfg_samples = sfg.sample(n)
+	plt.figure(figsize=(7, 7))
+	sigma2_range = [np.power(10.0, x) for x in np.arange(-6, 6)]
+	num_subplot_rows = np.ceil(np.sqrt(len(sigma2_range)))
+	for ii, sigma2 in enumerate(sigma2_range):
+		sfg = StandardFisherGaussian(p=p, sigma2=sigma2)
+		sfg_samples = sfg.sample(n)
 
-        plt.subplot(2, 2, ii + 1)
-        plt.scatter(sfg_samples[:, 0], sfg_samples[:, 1])
-        plt.title("sigma2={}".format(sigma2))
+		plt.subplot(num_subplot_rows, num_subplot_rows, ii + 1)
+		plt.scatter(sfg_samples[:, 0], sfg_samples[:, 1])
+		plt.title("sigma2={}".format(sigma2))
 
-    plt.show()
+	plt.show()
 
