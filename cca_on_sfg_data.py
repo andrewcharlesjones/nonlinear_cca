02d092f0cab4f8b1b6b148f404d6456f42e54028
Andy Jones <ajones788@gmail.com>
2020-10-17 07:29:23 -0500

    add first CCA/SFG experiment

---
+++ cca_on_sfg_data.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from standard_fisher_gaussian import StandardFisherGaussian
+from scipy.stats import multivariate_normal as mvn
+from sklearn.cross_decomposition import CCA
+from scipy.stats import pearsonr
+import seaborn as sns
+import pandas as pd
+
+
+if __name__ == "__main__":
+    n = 1000
+    p1 = 30
+    p2 = 30
+    k = 5  # latent dimension
+    sigma2 = 1
+    Psi1 = 1  # covariance of data1
+    Psi2 = 1  # covariance of data2
+
+    reps = 6  # num repeated experiments
+
+    # Vary sigma2 parameter - we expect to see traditional CCA behavior as sigma2 approaches 1/k
+    sigma2_range = [np.power(10.0, x) for x in np.arange(-6, 6)]
+    corr_list = np.zeros((reps, len(sigma2_range)))
+
+    for ii in range(reps):
+        for jj, sigma2 in enumerate(sigma2_range):
+
+            # Simulate data from SFG
+            sfg = StandardFisherGaussian(p=k, sigma2=sigma2)
+            z = sfg.sample(n)  # latent variables
+            W1 = np.random.normal(size=(k, p1))  # loadings
+            W2 = np.random.normal(size=(k, p2))  # loadings
+
+            # Sample two datasets for CCA
+            X1 = np.array([mvn.rvs(mean=zi@W1, cov=Psi1) for zi in z])
+            X2 = np.array([mvn.rvs(mean=zi@W2, cov=Psi2) for zi in z])
+
+            # Run "classic" CCA
+            cca = CCA(n_components=k)
+            cca.fit(X1, X2)
+
+            # Rotate true and fitted factors (using SVD) to make them more comparable
+            uz, dz, vz = np.linalg.svd(W1.T, full_matrices=False)
+            ucca, dcca, vcca = np.linalg.svd(
+                cca.x_loadings_, full_matrices=False)
+
+            true_factors = z @ vz.T @ np.diag(dz).T
+            cca_factors = cca.x_scores_ @ vcca.T @ np.diag(dcca).T
+
+            # Compute correlation between learned factors and true factors
+            corrs_for_heatmap = np.zeros((k, k))
+            for kii in range(k):
+                for kjj in range(k):
+                    curr_corr = np.abs(
+                        pearsonr(true_factors[:, kii], cca_factors[:, kjj])[0])
+                    corrs_for_heatmap[kii, kjj] = np.abs(curr_corr)
+            # sns.heatmap(corrs_for_heatmap)
+            # plt.show()
+
+            # Take the maximum of these correlations as a measure of performance (probably need to change this)
+            curr_corr = np.abs(
+                pearsonr(true_factors[:, 0], cca_factors[:, 0])[0])
+            corr_list[ii, jj] = np.max(corrs_for_heatmap)
+            print("Sigma2 = {}, Correlation = {}".format(
+                sigma2, round(np.max(corrs_for_heatmap), 3)))
+
+    # Plot performance across different values of sigma2
+    plt.errorbar(sigma2_range, np.mean(corr_list, 0),
+                 yerr=np.std(corr_list, 0), ecolor="purple")
+    plt.axvline(1/k, linestyle='--', c='red')
+    ax = plt.gca()
+    ax.set_xscale('log')
+    plt.xlabel("sigma2")
+    plt.ylabel("Performance")
+
+    plt.text(1/k, ax.get_ylim()[1], '1/k')
+    plt.show()
+    import ipdb
+    ipdb.set_trace()
